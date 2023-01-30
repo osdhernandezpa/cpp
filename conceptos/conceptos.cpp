75eb4f1ad1f502a48add83e66dcd562bc429a1f9
Oscar <osdhernandezpa@unal.edu.co>
2023-01-30 08:21:32 -0500

    lo que se me olvido subir

---
+++ conceptos/conceptos.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+
+int main(int argc, char **argv){
+
+    //los conceptos permiten limitar type param de una plantilla
+    //
+    //
+    //
+    //voy a dejar hasta aqui con c++ por ahora.
+    //si veo que necesito mas vuelvo.
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
